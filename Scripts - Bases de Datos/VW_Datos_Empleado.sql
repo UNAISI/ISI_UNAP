84f73537293db60bb54605a4d4368f9972852af4
UNAISI <ing.sistemas.atriis@gmail.com>
2015-10-03 20:35:21 -0600

    Revert "BD - Scripts"
    This reverts commit 2ee7a135047a0727d55566190332f7620b682de9.

--- Scripts - Bases de Datos/VW_Datos_Empleado.sql
+++
@@ -1,4 +0,0 @@
-create view vw_Datos_Empleado
-as
-select e.nombre as Nombre, e.apellido as Apellido, d.nombre as Departamento,c.nombreCargo as Cargo, e.estado as Estado
-from Departamento d inner join Empleado e on d.idDepartamento = e.idDepartamento inner join Cargo c on c.idCargo=e.idCargo
